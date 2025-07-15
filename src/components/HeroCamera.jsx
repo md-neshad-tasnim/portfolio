16f4389ae51816975e3720f850a8699e809ebb32
Neshad <djneshad@gmail.com>
2025-07-15 23:57:51 +0600

    3d working fck errors

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useFrame } from '@react-three/fiber';
+import { easing } from 'maath';
+import React, { useRef } from 'react'
 
-const HeroCamera = ({ children }) => {
+const HeroCamera = ({ children, isMobile }) => {
+  const groupRef = useRef();
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 40], 0.25, delta);
+    if (!isMobile) {
+      easing.dampE(groupRef.current.rotation, [-state.pointer.y / 3, -state.pointer.x / 5, 0], 0.25, delta);
+    }
+  })
+
+
   return (
-    <grouop>{children}</grouop>
+    <group ref={groupRef}>{children}</group>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,9 +4,61 @@ import React, { Suspense } from 'react'
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
 import { Center, OrbitControls, Bounds } from '@react-three/drei'
-import { Leva } from 'leva'
+import { Leva, useControls } from 'leva'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants'
+import Target from '../components/Target'
+import ReactLogo from '../components/ReactLogo'
+import Cube from '../components/Cube'
+import Rings from '../components/Rings'
+import HeroCamera from '../components/HeroCamera'
+
 
 const Hero = () => {
+
+  const x = useControls('HackerRoom', {
+    positionX: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    positionY: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    positionZ: {
+      value: 2.5,
+      min: -10,
+      max: 10
+    },
+    rotationX: {
+      value: 0,
+      min: -10,
+      max: 10
+    },
+    rotationY: {
+      value: 0,
+      min: -10,
+      max: 10
+    },
+    rotationZ: {
+      value: 0,
+      min: -10,
+      max: 10
+    },
+    scale: {
+      value: 1,
+      min: 0.1,
+      max: 10
+    }
+  })
+
+  const isSmall = useMediaQuery({ maxWidth: 440 })
+  const isMobile = useMediaQuery({ maxWidth: 768 })
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
 
   return (
@@ -16,19 +68,42 @@ const Hero = () => {
         <p className='hero_tag text-gray_gradient'>Building things for the web</p>
       </div>
       <div className="w-full h-full absolute inset-0">
-        <Leva />
-        <Canvas camera={{ position: [0, 2, 20], fov: 35 }} className='w-full h-full'>
+        <Leva hidden />
+
+        {/* canvas camera position camera={{ position: [0, 2, 20], fov: 35 }} */}
+        <Canvas className='w-full h-full'>
           <Suspense fallback={<CanvasLoader />}>
 
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-            <Bounds fit clip observe margin={1.2}>
+            {/* <Bounds fit clip observe margin={1.2}> */}
+            {/* <Bounds fit clip observe> */}
+            {/* <HeroCamera> */}
+            <HackerRoom
 
-              <HackerRoom scale={0.07} position={[0, 0, 0]} rotation={[0, 280, 0]} />
+              // position={[1.1, -6.7, 1.9]}
+              // rotation={[0.4, 3.1, -6.3]}
+              // scale={[0.1, 0.1, 0.1]}
+              position={[1.9, -7, -1.9]}
+              rotation={[0, -Math.PI, 0]}
+              scale={[0.1, 0.1, 0.1]}
+            />
 
-              <ambientLight intensity={1} />
-              <directionalLight position={[10, 10, 5]} intensity={1} />
-            </Bounds>
-            <OrbitControls />
+            {/* </HeroCamera> */}
+
+
+
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              {/* <Rings position={sizes.ringPosition} /> */}
+            </group>
+
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+            {/* </Bounds> */}
+            {/* <OrbitControls /> */}
           </Suspense>
         </Canvas>
 
